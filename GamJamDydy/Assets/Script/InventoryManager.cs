baf3c0891e26b34f1e97ce4002952606e1f01466
Damsss <damien.cuiec-aime@gadz.org>
2020-10-24 01:37:23 -0400

    ajout Inventaire, Interface

---
+++ GamJamDydy/Assets/Script/InventoryManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InventoryManager : MonoBehaviour
+{
+    public List<Item> items;
+    public int capacity;
+    public UnityEvent OnInventoryChanged;
+
+    private void Start()
+    {
+        items = new List<Item>(capacity);
+        if (OnInventoryChanged == null)
+        {
+            OnInventoryChanged = new UnityEvent();
+        }
+            
+    }
+
+    public void AddItem(Item item)
+    {
+        if (items.Count<capacity)
+        {
+            items.Add(item);
+        }
+
+        OnInventoryChanged.Invoke();
+
+    }
+
+
+
+}
---
+++ GamJamDydy/Assets/Script/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public InventoryManager playerInventory;
+    public Button slot1;
+    public Button slot2;
+
+    int nbBille;
+
+    
+
+    public void UpdateInventoryUI()
+    {
+        TextMeshProUGUI textSlot1 = slot1.GetComponentInChildren<TextMeshProUGUI>();
+
+        
+        nbBille = 0;
+        textSlot1.text = nbBille.ToString();
+        slot1.image.sprite = null;
+        slot2.image.sprite = null;
+
+        for (int i = 0; i < playerInventory.items.Count; i++)
+        {
+            if (playerInventory.items[i].GetName()=="Marble")
+            {
+                nbBille += 1;
+                slot1.image.sprite = playerInventory.items[i].GetSprite();
+            }
+
+            if (playerInventory.items[i].GetName() == "Key")
+            {
+                slot2.image.sprite = playerInventory.items[i].GetSprite();
+            }
+        }
+    }
+}
